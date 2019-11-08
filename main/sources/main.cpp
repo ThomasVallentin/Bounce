946eb8a67552cd4fd4ce68e4bf40236a40323315
Thomas <vallentin.thomas01@gmail.com>
2019-11-08 22:15:15 +0100

    first ppm gradient

---
+++ main/sources/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World, Bounce render !" << endl;
+	int xMax = 100;
+	int yMax = 50;
+
+	ofstream imageStream("D:/REPOSITORIES/Bounce/main/imageOutputs/test_gradient.ppm");
+
+	imageStream << "P3\n" << xMax << " " << yMax << "\n255\n";
+
+	for (int y = 0; y < yMax; y++) {
+		for (int x = 0; x < xMax; x++) {
+		
+			int r = float(x) / float(xMax) * 255.99;
+			int g = float(y) / float(yMax) * 255.99;
+			int b = 0.2 * 255.99;
+
+			imageStream << r << " " << g << " " << b << "\n";
+		}
+	}
+}
