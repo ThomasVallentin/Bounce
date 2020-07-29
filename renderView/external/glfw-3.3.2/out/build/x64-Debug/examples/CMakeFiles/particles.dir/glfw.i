6685329fef2206874a2536f334477295a63f5950
Thomas Vallentin <vallentin.thomas01@gmail.com>
2020-07-29 22:20:04 +0200

    Init proper renderView project glfw version 3.3.2 glew 2.2.0

---
+++ renderView/external/glfw-3.3.2/out/build/x64-Debug/examples/CMakeFiles/particles.dir/glfw.i
@@ -0,0 +1,4 @@
+#line 1 "D:\\REPO\\Bounce\\renderView\\external\\glfw-3.3.2\\examples\\glfw.rc"
+
+GLFW_ICON               ICON            "glfw.ico"
+
---
+++ renderView/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    std::cout << "Hello world !" << std::endl;
+}
