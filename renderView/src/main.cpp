5b7054bfb8b74f5d1f33322ea6e2a772a84346d7
Thomas Vallentin <vallentin.thomas01@gmail.com>
2020-07-29 22:40:45 +0200

    [UPDATE] CMakeList.txt : added DGLEW_STATIC + other missing definitions that caused the build to fail [UPDATE] main.cpp : Init window

--- renderView/src/main.cpp
+++ renderView/src/main.cpp
@@ -8,5 +8,40 @@
 
 int main()
 {
-    std::cout << "Hello world !" << std::endl;
+    // == GLFW initialization ==========================================================================================
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window;
+    window = glfwCreateWindow(1200, 600, "Bounce Render View", nullptr, nullptr);
+    if ( window==nullptr)
+    {
+        std::cout << "Failed to create a GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // == GLEW initialization ==========================================================================================
+    glfwMakeContextCurrent(window);
+    if ( glewInit() != GLEW_OK )
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // Main window loop
+    do{
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    while (glfwWindowShouldClose(window) == 0);
+
 }
